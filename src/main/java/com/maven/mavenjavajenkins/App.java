175bf62b52aab01c3ca20fedec43f71300076cc6
manoj kalvakolanu <manojkalvakolanu5@gmail.com>
2022-12-08 11:34:42 +0530

    maven java jenkins

---
+++ src/main/java/com/maven/mavenjavajenkins/App.java
@@ -0,0 +1,13 @@
+package com.maven.mavenjavajenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
